fee9450a0bab12d4a675fc18e9ec30b7da52747d
AJ <AJALCARDEAVILES@GMAIL.COM>
2026-06-12 20:47:41 +0800

    Added the collection file

---
+++ IStudentDataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using StudentManagementSystemModels;
+
+namespace StudentManagementSystemDataService
+{
+    public interface IStudentDataService
+    {
+        List<Student> GetStudents();
+        List<Student> GetStudentsByStatus(string status);
+        Student? SearchStudentInDb(string name);
+        void AddStudent(Student student);
+        void UpdateStatusById(int id, string newStatus);
+        void DeleteStudentById(int id);
+    }
+}
